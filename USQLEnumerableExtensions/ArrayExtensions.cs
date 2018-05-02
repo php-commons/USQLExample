e9bde73bf969a563c6d9b85a72bb4529442df1a1
ndykman <nathan.dykman@providence.org>
2018-05-02 10:19:32 -0700

    Initial version of example code.

---
+++ USQLEnumerableExtensions/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace USQLEnumerableExtensions
+{
+    /// <summary>
+    /// Make it easier to select a random object from an array. 
+    /// </summary>
+    public class ArrayExtensions
+    {
+        private static readonly Random _random = new Random();
+
+        public static T Random<T>(T[] values)
+        {
+            return values[_random.Next(0, values.Length)];
+        }
+    }
+}
